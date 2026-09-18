aa26a552afe69c2fc9b10e9d87f6b51cbf3ad613
007 <dr007cc@gmail.com>
2026-09-18 13:21:48 +0330

    fix: service

--- src/utils.rs
+++ src/utils.rs
@@ -2,14 +2,13 @@ use std::process::Command;
 
 pub fn do_ls(arg: &str) -> Vec<String> {
     let output = Command::new("find")
-        .args(["-type", "f", arg])
+        .args(["-type", "f", "-L", arg])
         .output()
-        .expect("failed to execute ls");
+        .expect("failed to execute find");
 
-    let files: Vec<String> = String::from_utf8_lossy(&output.stdout)
-        .lines()
-        .map(|s| s.to_string())
-        .collect();
+    let output = String::from_utf8_lossy(&output.stdout);
+    log::info!("find output: {output}");
+    let files: Vec<String> = output.lines().map(|s| s.to_string()).collect();
 
     files
 }
